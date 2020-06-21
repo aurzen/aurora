12dc3f97419747dfc4a438c7f2b2b8973a8a0e66
Zenith <z@zenith.dev>
2020-06-21 13:08:18 -0700

    aurcore 0.1.18

--- aurcore/event/__init__.py
+++ aurcore/event/__init__.py
@@ -109,6 +109,7 @@ class EventMuxer(AutoRepr):
 
     def add_listener(self, func: ty.Union[EventFunction, EventWaiter]):
         container = self.waiters if isinstance(func, EventWaiter) else self.funcs
+        print(f"adding {func}")
         container.add(func)
         # self.one_times.add(one_time)
 
