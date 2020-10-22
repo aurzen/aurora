786380790a70a2989955436c2752d78519b64bac
Zenith <z@zenith.dev>
2020-10-22 14:27:15 -0700

    aurcore 1.1.41

--- aurcore/__init__.py
+++ aurcore/__init__.py
@@ -6,7 +6,9 @@ from . import util
 from . import log
 
 from loguru import logger
+
 log.setup()
+
 
 def aiorun(startup, cleanup):
    loop = asyncio.get_event_loop()
@@ -14,16 +16,17 @@ def aiorun(startup, cleanup):
       loop.create_task(startup)
       loop.run_forever()
    except KeyboardInterrupt:
-      logger.info("KeyboardInterrupt detected. Cleaning up.")
-      loop.run_until_complete(cleanup)
-      all_tasks = asyncio.gather(*asyncio.all_tasks(loop), return_exceptions=True)
-      all_tasks.cancel()
-      with contextlib.suppress(asyncio.CancelledError):
-         loop.run_until_complete(all_tasks)
-      loop.run_until_complete(loop.shutdown_asyncgens())
 
-      logger.info("Cleaned up! Shutting down.")
-      loop.close()
+      with contextlib.suppress(asyncio.CancelledError):
+         logger.info("KeyboardInterrupt detected. Cleaning up.")
+         loop.run_until_complete(cleanup)
+         all_tasks = asyncio.gather(*asyncio.all_tasks(loop), return_exceptions=True)
+         all_tasks.cancel()
+         loop.run_until_complete(all_tasks)
+         loop.run_until_complete(loop.shutdown_asyncgens())
+
+         logger.info("Cleaned up! Shutting down.")
+         loop.close()
 
 
 __all__ = ["Event", "Eventful", "EventRouterHost", "EventRouter", "log", "util"]
